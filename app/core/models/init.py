fe0ef5113db66338061cf8a4e8d08f47c258d5b0
Sven246 <svenolafduden@gmail.com>
2026-02-03 12:37:59 +0300

    Create init.py

---
+++ app/core/models/init.py
@@ -0,0 +1,7 @@
+from .user import User
+from .post import Post
+from .comment import Comment
+from .story import Story
+from .coin_transaction import CoinTransaction
+from .premium_subscription import PremiumSubscription
+from .ad import Ad
