630853b898a75fe8d6ba3e12c8b0b12238d88d93
Sven246 <svenolafduden@gmail.com>
2026-02-04 08:07:34 +0300

    Delete app/core/models/init.py

--- app/core/models/init.py
+++
@@ -1,7 +0,0 @@
-from .user import User
-from .post import Post
-from .comment import Comment
-from .story import Story
-from .coin_transaction import CoinTransaction
-from .premium_subscription import PremiumSubscription
-from .ad import Ad
