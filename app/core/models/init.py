82c8800438ca4232babcf40b4433e751c66f8f0c
Sven246 <svenolafduden@gmail.com>
2026-02-04 08:08:16 +0300

    Create init.py

---
+++ app/core/models/init.py
@@ -0,0 +1,14 @@
+from .user import User
+from .post import Post
+from .comment import Comment
+from .story import Story
+from .coin_transaction import CoinTransaction
+from .premium_subscription import PremiumSubscription
+from .ad import Ad
+
+from .channel import Channel
+from .channel_post import ChannelPost
+from .channel_subscription import ChannelSubscription
+from .channel_moderator import ChannelModerator
+from .channel_reaction import ChannelReaction
+from .channel_analytics import ChannelAnalytics
